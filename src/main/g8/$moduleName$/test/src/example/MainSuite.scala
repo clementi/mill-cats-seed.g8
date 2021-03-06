37041daf38e3f06a1df8937212a2057dd7614813
Jeff Pratt <me@jpratt.dev>
2021-03-06 15:18:12 -0700

    Update and rename HelloSuite.scala to MainSuite.scala

--- src/main/g8/$moduleName$/test/src/example/MainSuite.scala
+++ src/main/g8/$moduleName$/test/src/example/MainSuite.scala
@@ -1,13 +1,9 @@
 package example
 
-class HelloSuite extends munit.FunSuite {
+class MainSuite extends munit.FunSuite {
   test("numbers") {
     val obtained = 42
     val expected = 43
     assertEquals(obtained, expected)
-  }
-
-  test("hello") {
-    assertEquals(Hello.greeting, "hello")
   }
 }
