0da06f6dc5727630ae8616ebee675d1da9d78451
Jeff Pratt <me@jpratt.dev>
2021-03-06 15:18:30 -0700

    Update and rename Hello.scala to Main.scala

--- src/main/g8/$moduleName$/src/example/Main.scala
+++ src/main/g8/$moduleName$/src/example/Main.scala
@@ -3,7 +3,7 @@ package example
 import cats.effect.{IOApp, ExitCode}
 import cats.effect.Console.io._
 
-object Hello extends IOApp {
+object Main extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     putStrLn("Hello").as(ExitCode.Success)
   }
